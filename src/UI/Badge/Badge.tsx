4babdf333bc7367dd7453d0e524779852cfd8227
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-20 19:10:20 +0300

    add Badge

---
+++ src/UI/Badge/Badge.tsx
@@ -0,0 +1,27 @@
+import styles from "./Badge.module.css";
+import { BadgeProps } from "../../utils/types.js";
+import Remove from "../../assets/images/icon-remove.svg";
+
+export const Badge = (props: BadgeProps) => {
+  const {
+    variant = "basic",
+    colorScheme = "light",
+    onClick,
+    children,
+    onClear,
+  } = props;
+
+  return (
+    <div
+      className={`${styles.badge} ${styles[variant]} ${styles[colorScheme]}`}
+      onClick={onClick}
+    >
+      <span>{children}</span>
+      {variant === "clearable" && (
+        <div className={styles.badge_remover} onClick={onClear}>
+          <img src={Remove} alt="icon-remove" />
+        </div>
+      )}
+    </div>
+  );
+};
