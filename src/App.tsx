648c3b151dab9c7e443487c24884d5c9097fac5a
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-20 18:07:05 +0300

    modify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Header } from "./components/Header/Header.js";
+import { FilterPanel } from "./components/FilterPanel/FilterPanel.js";
+import { JobList } from "./components/JobList/JobList.js";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="container">
+        <FilterPanel />
+        <JobList />
+      </div>
     </>
   );
 }
