56801a2ea8d510ec6f4fcb209713ec519b438058
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-20 19:38:40 +0300

    add FilterPanel

---
+++ src/components/FilterPanel/FilterPanel.tsx
@@ -0,0 +1,19 @@
+import styles from "./FilterPanel.module.css";
+import { Badge } from "../../UI/Badge/Badge.js";
+import { Card } from "../../UI/Card/Card.js";
+import { Stack } from "../../UI/Stack/Stack.js";
+
+export const FilterPanel = () => {
+  return (
+    <Card>
+      <div className={styles.container}>
+        <Stack>
+          <Badge variant="clearable">Frontend</Badge>
+          <Badge variant="clearable">Backend</Badge>
+          <Badge variant="clearable">React</Badge>
+        </Stack>
+        <button className={styles.link}>Clear</button>
+      </div>
+    </Card>
+  );
+};
