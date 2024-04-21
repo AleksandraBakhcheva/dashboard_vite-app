a2de9f28f81bf9f6a05c583825b06537e7969311
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-21 19:18:03 +0300

    add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header.js";
 import { FilterPanel } from "./components/FilterPanel/FilterPanel.js";
 import { JobList } from "./components/JobList/JobList.js";
+import { Footer } from "./components/Footer/Footer.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <FilterPanel />
         <JobList />
       </div>
+      <Footer />
     </>
   );
 }
