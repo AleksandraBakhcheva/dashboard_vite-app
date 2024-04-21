f6097e0026571daa45f1068637665068402c8566
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-21 19:36:33 +0300

    add h1 to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import styles from "./Header.module.css";
 
 export const Header = () => {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <h1>Job Listings Filter</h1>
+    </header>
+  );
 };
