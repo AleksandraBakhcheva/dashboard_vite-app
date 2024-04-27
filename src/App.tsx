c06c759511f1091f5a10dba0337732f626c9629d
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-28 00:54:54 +0500

    modify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addPositions } from "./store/positions/positionActions.js";
+import data from "./mock/data.json";
 import { Header } from "./components/Header/Header.js";
 import { FilterPanel } from "./components/FilterPanel/FilterPanel.js";
 import { JobList } from "./components/JobList/JobList.js";
 import { Footer } from "./components/Footer/Footer.js";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addPositions(data));
+  }, []);
+
   return (
     <>
       <Header />
