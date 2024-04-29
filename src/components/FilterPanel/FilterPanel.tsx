9e44bc4b288fae38892758d12c883337db5f5f7b
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-04-29 20:14:00 +0500

    add state to components

--- src/components/FilterPanel/FilterPanel.tsx
+++ src/components/FilterPanel/FilterPanel.tsx
@@ -1,18 +1,42 @@
 import styles from "./FilterPanel.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectFilters } from "../../store/filters/filterSelectors.js";
+import {
+  removeFilter,
+  clearFilter,
+} from "../../store/filters/filterActions.js";
 import { Badge } from "../../UI/Badge/Badge.js";
 import { Card } from "../../UI/Card/Card.js";
 import { Stack } from "../../UI/Stack/Stack.js";
 
 export const FilterPanel = () => {
+  const currentFilters = useSelector(selectFilters);
+  const dispatch = useDispatch();
+
+  if (currentFilters.length === 0) {
+    return null;
+  }
+
   return (
     <Card>
       <div className={styles.filterPanel}>
         <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+          {currentFilters.map((filter) => (
+            <Badge
+              key={filter}
+              variant="clearable"
+              onClear={() => dispatch(removeFilter(filter))}
+            >
+              {filter}
+            </Badge>
+          ))}
         </Stack>
-        <button className={styles.filterPanel_link}>Clear</button>
+        <button
+          className={styles.filterPanel_link}
+          onClick={() => dispatch(clearFilter)}
+        >
+          Clear
+        </button>
       </div>
     </Card>
   );
--- src/components/JobList/JobList.tsx
+++ src/components/JobList/JobList.tsx
@@ -1,12 +1,38 @@
 import styles from "./JobList.module.css";
-import data from "../../mock/data.json";
+import { useSelector, useDispatch } from "react-redux";
+import { selectVisiblePositions } from "../../store/positions/positionSelectors.js";
+import { selectFilters } from "../../store/filters/filterSelectors.js";
+import { addFilter } from "../../store/filters/filterActions.js";
 import { JobPosition } from "../JobPosition/JobPosition.js";
+import { JobPositionProps } from "../../utils/types.js";
 
 export const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFilters: string[] = useSelector(selectFilters);
+
+  const positions = useSelector(
+    (state: {
+      positions: {
+        role: string;
+        level: string;
+        languages: string[];
+        tools: string[];
+      }[];
+    }) => selectVisiblePositions(state, currentFilters)
+  );
+
+  const handleAddFilter = (filter: string) => {
+    dispatch(addFilter(filter));
+  };
+
   return (
     <div className={styles.jobList}>
-      {data.map((item) => (
-        <JobPosition key={item.id} {...item} />
+      {positions.map((item: JobPositionProps) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
   );
