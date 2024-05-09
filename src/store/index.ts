1cd1ef61fa0d314ce4101e2e5f22603b0fe276ff
AleksandraBakhcheva <alexandra.bakhcheva@gmail.com>
2024-05-09 19:14:10 +0500

    update store with storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
 import { rootReducer } from "./rootReducer.js";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = createStore(
-  rootReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export { store };
+export const persistor = persistStore(store);
