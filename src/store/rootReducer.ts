a8ce0af62df66023fd0cb626526928e546a83bec
AleksandraBakhcheva <alexandra.bakhcheva@gmail.com>
2024-05-09 19:16:07 +0500

    add type for rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,11 @@
-import { combineReducers } from "redux";
+import { combineReducers, Reducer } from "redux";
 import { positionReducer } from "./positions/positionReducer.js";
 import { filterReducer } from "./filters/filterReducer.js";
 
 export const rootReducer = combineReducers({
   positions: positionReducer,
   filters: filterReducer,
-});
+}) as Reducer<
+  Partial<{ positions: string[]; filters: string[] }>,
+  { type: string; positions: string[] } | { type: string; filter: string }
+>;
